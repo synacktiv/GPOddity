ae8f1cba4b8d5e6eb8895e428158e88fb9ad0217
quentin <quentin.roland@synacktiv.com>
2023-09-10 12:57:15 +0200

    Fix #2 (gpt.ini encoding errors)

--- helpers/version_utils.py
+++ helpers/version_utils.py
@@ -10,8 +10,8 @@ def update_GPT_version_number(ldap_session, gpo_dn, gpo_type):
         updated_version = versionNumber + 1
     else:
         updated_version = versionNumber + 65536
-    with open(os.path.join(OUTPUT_DIR, "gpt.ini"), 'r') as f:
+    with open(os.path.join(OUTPUT_DIR, "gpt.ini"), 'r', errors='surrogateescape') as f:
         content = f.read()
     new_content = re.sub('=[0-9]+', '={}'.format(updated_version), content)
-    with open(os.path.join(OUTPUT_DIR, "gpt.ini"), 'w') as f:    
+    with open(os.path.join(OUTPUT_DIR, "gpt.ini"), 'w', errors='surrogateescape') as f:    
         f.write(new_content)
